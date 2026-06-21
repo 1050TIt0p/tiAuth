e1d0e7e721392ba64ac80246f78d89abfe1c34b9
1050TIt0p <160386710+1050TIt0p@users.noreply.github.com>
2026-06-21 09:22:11 +0300

    refactor: remove commented lines

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,6 @@ subprojects {
     pluginManager.apply("java")
 
     dependencies {
-        //implementation("com.github.1050TIt0p:NanoLimbo:1.12.0-3")
         implementation("com.j256.ormlite:ormlite-jdbc:6.1")
         implementation("com.zaxxer:HikariCP:7.1.0")
         implementation("at.favre.lib:bcrypt:0.10.2")
