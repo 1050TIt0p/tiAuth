55cf011e502038e6b69c706a47310ff32d05db2e
1050TIt0p <160386710+1050TIt0p@users.noreply.github.com>
2026-06-21 17:37:30 +0300

    fix: sha256

--- common/src/main/java/ru/matveylegenda/tiauth/hash/HashFactory.java
+++ common/src/main/java/ru/matveylegenda/tiauth/hash/HashFactory.java
@@ -3,6 +3,7 @@ package ru.matveylegenda.tiauth.hash;
 import ru.matveylegenda.tiauth.hash.impl.Argon2Hash;
 import ru.matveylegenda.tiauth.hash.impl.BCryptHash;
 import ru.matveylegenda.tiauth.hash.impl.Sha256AuthMeHash;
+import ru.matveylegenda.tiauth.hash.impl.Sha256Hash;
 
 public class HashFactory {
     public static Hash create(HashType hashType) {
@@ -10,7 +11,7 @@ public class HashFactory {
             case BCRYPT -> new BCryptHash();
             case SHA256 -> new Sha256AuthMeHash();
             case ARGON2 -> new Argon2Hash();
-            case SHA256_DEFAULT ->
+            case SHA256_DEFAULT -> new Sha256Hash();
         };
     }
 }
